69c6cd05f528fb1975ad8cbffdb35ffd4cca8902
unknown <mahadihasanpm@gmail.com>
2025-08-17 04:10:51 +0600

    added

--- main.py
+++ main.py
@@ -13,12 +13,9 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
 models.Base.metadata.create_all(bind = engine)
 
 app.include_router(router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
